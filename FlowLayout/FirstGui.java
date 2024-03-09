5fa64815e7f0239019e0cc1af9fc6991ca338366
themohan69 <mohanthedon69@gmail.com>
2024-03-09 08:39:33 +0530

    Hmm

---
+++ FlowLayout/FirstGui.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+public class FirstGui{
+    public static void main(String [] args){
+        JFrame newFrame = new JFrame("First Frame");
+        newFrame.setSize(500, 500);
+        newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTextField t = new JTextField("Write What You Can!");
+        JButton b0 = new JButton("Click Me!");
+        JButton b1 = new JButton("Click Me!");
+        JButton b2 = new JButton("Click Me!");
+        JButton b3 = new JButton("Click Me!");
+        // JPanel p = new JPanel();
+        newFrame.setLayout(new FlowLayout());
+        newFrame.add(t);
+        newFrame.add(b0);
+        newFrame.add(b1);
+        newFrame.add(b2);
+        newFrame.add(b3);
+        // newFrame.add(p);
+        newFrame.setVisible(true);
+    }
+}
---
+++ FlowLayout/panel.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class panel {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("First Page");
+        JTextField t = new JTextField("Hello");
+        JButton b = new JButton("Click Me");
+        JPanel p = new JPanel();
+        p.add(t);
+        p.add(b);
+        f.add(p);
+        b.setBounds(200, 100, 50, 50);
+        f.setSize(500, 500);
+        f.setVisible(true);
+        f.setLayout(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
